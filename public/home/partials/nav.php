e99c88308ce53c22f435ccc7a66a37d8eab0155b
Your Name <you@example.com>
2024-08-16 05:12:27 +0100

    made some changes

--- public/home/partials/nav.php
+++
@@ -1,7 +0,0 @@
-<li><a href="/">Home</a></li>
-<li><a href="login">Login</a></li>
-<li><a href="register">Register</a></li>
-<li><a href="why-us.php">Why <?= $domain ?></a></li>
-<li><a href="about.php">About us</a></li>
-<li><a href="contact.php">Customer service</a></li>
-<li><a href="faqs.php">FAQs</a></li>
--- public/localhost.php
+++
@@ -1,4 +0,0 @@
-<?php
-$domain = 'Block WavePlus';
-$phone = '+1 (308) 636-2404';
-$email = 'support@blockwaveplus.com';
